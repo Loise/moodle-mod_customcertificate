35cecb489c6aceb9908e5aabec5b9d1dd05e20e5
loise.fenoll <myuuu@live.fr>
2014-04-07 16:07:34 +0200

    mis à jour de version en 1.0.0

--- version.php
+++ version.php
@@ -12,6 +12,6 @@
 $module->version  = 2013070501; // The current module version (Date: YYYYMMDDXX)
 $module->requires = 2011120500;  // Requires this Moodle version
 $module->cron     = 4 * 3600;    // Period for cron to check this module (secs)
-$module->release  = '2.0.2';       // Human-friendly version name
+$module->release  = '1.0.0';       // Human-friendly version name
 //MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC, MATURITY_STABLE
 $module->maturity = MATURITY_STABLE;
